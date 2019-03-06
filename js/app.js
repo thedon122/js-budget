bb3d75bafa3162d1962c0bdff4d935d916678061
thedon122 <donovan122@yahoo.com>
2019-03-06 16:16:06 -0500

    Balance now shows

--- js/app.js
+++ js/app.js
@@ -29,11 +29,33 @@ class UI {
     }
     else {
       this.budgetAmount.textContent = value;
-      this.budgetInput.value = '';
+      this.budgetInput.value = "";
       this.showBalance();
     }
   }
-
+  // show balance
+  showBalance() {
+    const expense = this.totalExpense();
+    const total = parseInt(this.budgetAmount.textContent)- expense;
+    this.balanceAmount.textContent = total;
+    if(total < 0){
+      this.balance.classList.remove("showGreen", "showBlack");
+      this.balance.classList.add("showRed");
+    }
+    else if (total > 0){
+      this.balance.classList.remove("showRed", "showBlack");
+      this.balance.classList.add("showGreen");
+    }
+    else if (total === 0){
+      this.balance.classList.remove("showRed", "showGreen");
+      this.balance.classList.add("showBlack");
+    }
+  }
+  // total expense
+  totalExpense(){
+    let total = 400;
+    return total;
+  }
 }
 
 
